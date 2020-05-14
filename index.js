ac95cf8f3be78e5179bbc852dcca169e1469b917
Blueman <yzhang01@mica.edu>
2020-05-13 23:02:42 -0400

    update

--- index.js
+++ index.js
@@ -6,18 +6,25 @@ const io = require('socket.io')(port, {
 });
 
 var users = {}
+var light = {spot: .5, env: .5}
 
 io.on('connection', (client) => {
   client.on('onboard', (userData) => {
-    users[client.id] = {id: client.id, name: userData.name, realname: userData.realname, gender: userData.gender, data: {}}
+    users[client.id] = {id: client.id, name: userData.name, realname: userData.realname, gender: userData, data: {}}
     client.emit('existuser', users)
     io.emit('newuser', users[client.id])
+    client.emit('light', light)
   })
 
   client.on('move', data => {
     if(users[client.id]){
       users[client.id].data = data
     }
+  })
+
+  client.on('light', data => {
+    light = data
+    io.emit('light', light)
   })
   // disconnect
   client.on('disconnect', (data) => {
