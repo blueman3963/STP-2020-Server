f5ed02daeed66a974c03bfceebef77191d56ac21
Blueman <yzhang01@mica.edu>
2020-05-20 16:26:24 -0400

    update

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ const io = require('socket.io')(port, {
   pingTimeout: 60000
 });
 
+//
+const firebase = require('firebase')
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyDVL8oNN6oMXZT1HFGuzR-XxdRiP2PU9K0",
+  authDomain: "stp-data-30060.firebaseapp.com",
+  databaseURL: "https://stp-data-30060.firebaseio.com",
+  projectId: "stp-data-30060",
+  storageBucket: "stp-data-30060.appspot.com",
+  messagingSenderId: "908372679886"
+};
+
+//initialize firebase
+const firebaseApp = firebase.apps.length ? firebase.apps[0] : firebase.initializeApp(config);
+const db = firebaseApp.firestore();
+
 var users = {}
 var waiting = []
 var order = 0
@@ -19,10 +36,12 @@ setInterval(() => {
   }
 },3000)
 
+const threshold = 2
+
 io.on('connection', (client) => {
 
   client.on('ready', () => {
-    if(Object.keys(users).length < 2) {
+    if(Object.keys(users).length < threshold) {
       client.emit('ready')
     } else {
       waiting.push(client.id)
@@ -33,9 +52,18 @@ io.on('connection', (client) => {
   client.on('onboard', (userData) => {
     users[client.id] = {id: client.id, role: userData.role, realname: userData.first+' '+userData.last, data: {}}
     client.emit('exist', users)
+
     io.emit('newuser', users[client.id])
     client.emit('chathistory', chat)
     client.emit('artstart', start)
+    //get data
+    let logtime = Date.now()
+    db.collection('login').doc(logtime+'_'+client.id).set({
+      first: userData.first,
+      last: userData.last,
+      logtime: logtime
+    })
+    users[client.id].logtime = logtime
   })
 
   client.on('move', data => {
@@ -47,6 +75,14 @@ io.on('connection', (client) => {
   client.on('message', data => {
     chat.push({id:client.id, msg: data.data, name:data.name})
     io.emit('chathistory', chat)
+    if(chat.length >= 20) {
+      let newchat = chat.splice(10)
+      let time = Date.now()
+      db.collection('chat').doc('m'+time).set({
+        chat: chat
+      })
+      chat = newchat
+    }
     io.emit('message', {id:client.id, msg: data.data, name: data.name})
   })
 
@@ -63,10 +99,18 @@ io.on('connection', (client) => {
 
     if(users[client.id]) {
       io.emit('kill', client.id)
+      //get data
+      let leavetime = Date.now()
+      db.collection('login').doc(users[client.id].logtime+'_'+client.id).update({
+        leavetime: leavetime
+      })
       delete users[client.id]
 
-      let next = waiting.shift()
-      io.to(next).emit('ready')
+
+      if(Object.keys(users).length < threshold) {
+        let next = waiting.shift()
+        io.to(next).emit('ready')
+      }
     }
 
   });
